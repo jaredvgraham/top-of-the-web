49dede70805f2bf9d40853dacaeb2f9002bf0770
Evan Wilson <evanmwilson2123@gmail.com>
2024-07-23 13:23:21 -0400

    basic layout

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1 className="text-blue-500">Hero</h1>
+    </div>
+  );
+};
+
+export default Hero;
