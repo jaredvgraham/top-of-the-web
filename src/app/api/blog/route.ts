c40a30988e4690064cfec21ef047e71d2ad151b2
Evan Wilson <evanmwilson2123@gmail.com>
2024-07-26 11:28:53 -0400

    blog endpoint and page

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse, NextRequest } from "next/server";
+import dbConnect from "@/lib/db";
+import Blog from "@/models/Blog";
+import { BiLogoSlackOld } from "react-icons/bi";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  await dbConnect();
+
+  try {
+    const { title, content } = await req.json();
+    const blog = await Blog.create({ title, content });
+    return NextResponse.json(
+      { success: true, data: blog, message: "Blog created successfully" },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: error.status || 500 }
+    );
+  }
+}
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  await dbConnect();
+
+  try {
+    const blogs = await Blog.find();
+    return NextResponse.json(
+      { success: true, data: BiLogoSlackOld },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: error.status || 500 }
+    );
+  }
+}
