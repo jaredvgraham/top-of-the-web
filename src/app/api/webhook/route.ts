30ed0e3431bdaf80fa2130ccff2b4e541f1330c6
JaredvGraham <jgram153@icloud.com>
2024-07-25 23:59:22 -0400

    payment testing

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 //
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 console.log(process.env.STRIPE_WEBHOOK_SECRET!);
 
 export async function POST(req: NextRequest) {
@@ -11,18 +12,20 @@ export async function POST(req: NextRequest) {
   const payload = await req.text();
   const response = JSON.parse(payload);
 
-  const sig = req.headers.get("Stripe-Signature") as string;
+  const sig = req.headers.get("stripe-signature") as string;
   console.log("Signature received:", sig);
+
+  if (!sig || !webhookSecret) {
+    console.log("no sig or webhook secret");
+
+    return NextResponse.json({ status: 400, message: "Invalid signature" });
+  }
 
   const dateTime = new Date(response.created * 1000).toLocaleDateString();
   const timeString = new Date(response.created * 1000).toLocaleDateString();
   //cm
   try {
-    let event = stripe.webhooks.constructEvent(
-      payload,
-      sig,
-      process.env.STRIPE_WEBHOOK_SECRET!
-    );
+    let event = stripe.webhooks.constructEvent(payload, sig, webhookSecret);
     console.log("email", response.data.object.billilng_details.email);
 
     return NextResponse.json({ status: "success", event: event.type });
