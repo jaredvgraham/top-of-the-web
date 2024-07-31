267ef75116157308c45db6a9ea13673e0e881340
evanwilson2123 <evanmitchellwilson@gmail.com>
2024-07-31 18:26:16 -0400

    stripe monthly payments suck

--- src/app/api/stripe/subscription/route.ts
+++ src/app/api/stripe/subscription/route.ts
@@ -37,22 +37,22 @@ async function createPrice(pack: string) {
   let price: number;
   switch (pack) {
     case "Starter Website Package":
-      price = 3000;
+      price = 20000;
       break;
     case "Standard Website Package":
-      price = 15000;
+      price = 35000;
       break;
     case "Advanced Website Package":
-      price = 50000;
+      price = 75000;
       break;
     case "Enterprise Website Package":
-      price = 100000;
-      break;
-    case "super":
       price = 250000;
       break;
-    case "mega":
-      price = 500000;
+    case "E-Commerce Website Package":
+      price = 125000;
+      break;
+    case "test-installment":
+      price = 100;
       break;
     default:
       price = 0;
@@ -61,38 +61,111 @@ async function createPrice(pack: string) {
   return price;
 }
 
+async function createRecurringPrice(plan: string) {
+  let price: number;
+  switch (plan) {
+    case "hosting":
+      price = 3000;
+      break;
+    case "hosting seo":
+      price = 6000;
+      break;
+    case "basic ad":
+      price = 50000;
+      break;
+    case "standard ad":
+      price = 120000;
+      break;
+    case "advanced ad":
+      price = 300000;
+      break;
+    case "rapid growth ad":
+      price = 500000;
+      break;
+    case "test":
+      price = 100;
+      break;
+    default:
+      price = 0;
+  }
+  return price;
+}
+
 export async function POST(req: NextRequest) {
-  const { email, pack } = await req.json();
+  const { email, pack, plan } = await req.json();
   try {
     // create price
-    const price = await createPrice(pack);
-    if (price === 0) {
+    const websitePrice = await createPrice(pack);
+    if (websitePrice === 0) {
       return NextResponse.json({ message: "Invalid pack" }, { status: 400 });
     }
 
-    console.log("price created: ", price);
+    const recurringPrice = await createRecurringPrice(plan);
+    if (recurringPrice === 0) {
+      return NextResponse.json({ message: "Invalid plan" }, { status: 400 });
+    }
+
+    console.log("website price created: ", websitePrice);
+    console.log("recurring price created: ", recurringPrice);
 
     const stripePrice = await stripe.prices.create({
-      unit_amount: price,
+      unit_amount: websitePrice,
       currency: "usd",
       recurring: {
         interval: "month",
       },
+      metadata: {
+        type: "installment",
+      },
       product_data: {
         name: pack,
+      },
+    });
+    const recurringStripePrice = await stripe.prices.create({
+      unit_amount: recurringPrice,
+      currency: "usd",
+      recurring: {
+        interval: "month",
+      },
+      metadata: {
+        type: "indefinite",
+      },
+      product_data: {
+        name: plan,
       },
     });
 
     // get or create customer Id
     const customerId = await getOrCreateCustomerId(email);
 
+    const schedule = await stripe.subscriptionSchedules.create({
+      customer: customerId,
+      start_date: "now",
+      end_behavior: "release",
+      phases: [
+        {
+          items: [
+            {
+              price: stripePrice.id,
+              quantity: 1,
+            },
+          ],
+          iterations: 4,
+        },
+      ],
+    });
+
     // Create a checkout session for the purchase
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       customer: customerId,
       line_items: [
+        // {
+        //   price: stripePrice.id,
+        //   quantity: 1,
+        // },
         {
-          price: stripePrice.id,
+          price: recurringStripePrice.id,
           quantity: 1,
         },
       ],
--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,55 +1,3 @@
-// import Stripe from "stripe";
-// import { NextRequest, NextResponse } from "next/server";
-
-// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-// const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
-
-// export async function POST(req: NextRequest) {
-//   const payload = await req.text();
-//   const sig = req.headers.get("Stripe-Signature") as string;
-
-//   try {
-//     const event = stripe.webhooks.constructEvent(payload, sig, webhookSecret);
-
-//     if (event.type === "checkout.session.completed") {
-//       const session = event.data.object as Stripe.Checkout.Session;
-
-//       // Fetch line items for the session
-//       const lineItems = await stripe.checkout.sessions.listLineItems(
-//         session.id,
-//         {
-//           limit: 100,
-//         }
-//       );
-
-//       for (const item of lineItems.data) {
-//         if (!item.price) return;
-//         const product = await stripe.products.retrieve(
-//           item.price.product as string
-//         );
-//         const productName = product.name;
-//         const quantity = item.quantity;
-//         const amount = item.amount_total;
-
-//         console.log("Product Name", productName);
-//         console.log("Quantity", quantity);
-//         console.log("Amount", amount);
-//       }
-
-//       const email = session.customer_details?.email;
-//       const phone = session.customer_details?.phone;
-//       console.log("Customer phone: ", phone);
-
-//       console.log("Customer email: ", email);
-//     }
-
-//     return NextResponse.json({ status: "success", event: event.type });
-//   } catch (err: any) {
-//     console.log("Webhook Error", err.message);
-//     return NextResponse.json({ status: "error", message: err.message });
-//   }
-// }
-
 import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
 import Purchase from "@/models/Purchase";
@@ -57,6 +5,24 @@ import dbConnect from "@/lib/db";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+
+async function subscriptionScheduleExists(
+  customerId: string,
+  productId: string
+): Promise<boolean> {
+  const schedules = await stripe.subscriptionSchedules.list({
+    customer: customerId,
+  });
+
+  return schedules.data.some((schedule) =>
+    schedule.phases.some((phase) =>
+      phase.items.some((item) => {
+        const price = item.price as Stripe.Price;
+        return price.product === productId;
+      })
+    )
+  );
+}
 
 export async function POST(req: NextRequest) {
   await dbConnect();
@@ -81,10 +47,13 @@ export async function POST(req: NextRequest) {
       let amount = 0;
       for (const item of lineItems.data) {
         if (!item.price) return;
-        const product = await stripe.products.retrieve(
-          item.price.product as string
-        );
-        productName = product.name;
+
+        const price = item.price as Stripe.Price;
+        if (price.product && typeof price.product === "string") {
+          const product = await stripe.products.retrieve(price.product);
+          productName = product.name;
+        }
+
         const quantity = item.quantity;
         amount = item.amount_total;
 
@@ -114,4 +83,3 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ status: "error", message: err.message });
   }
 }
-///
